dcd3d0f992565e4c14bcf6cf8eb45c6a6fb0eead
p71 <piotr_chamera@poczta.onet.pl>
2014-05-14 11:56:32 +0200

    Added note about modification in preamble

--- codes.lisp
+++ codes.lisp
@@ -1,6 +1,7 @@
 ;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: CL-USER; Base: 10 -*-
 
 ;;; This software is Copyright (c) Stefan Scholl, 2005-2008.
+;;; Modified by Piotr Chamera, 2014.
 ;;; Stefan Scholl grants you the rights to distribute
 ;;; and use this software as governed by the terms
 ;;; of the Lisp Lesser GNU Public License
